8c264aef7ae38dcc9955d42e5834f7187c0bd9ef
Aryan Rohamare <125646823+trenoxx@users.noreply.github.com>
2024-08-14 10:45:40 +0530

    Add files via upload

--- as3.cpp
+++ as3.cpp
@@ -1,10 +1,15 @@
+/*Implement a problem of smallest number with at
+least n trailing zeroes in factorial.
+Statement: Given a number n. The task is to find the
+smallest number whose factorial contains at least n
+trailing zeroes.*/
 #include<iostream>
 #include<cmath>
 using namespace std;
 
 int main()
 {
-    int a,count,fact=1,n,total,i,x=0;
+    int a,count,fact=1,n,total,i,x=0,ans;
     cout<<"Enter value : ";
     cin>>n;
 
@@ -16,4 +21,15 @@ int main()
         }
     }
     cout<<count;
+    //subproblem 1 solved
+    //subproblem 2 is to cross check our answer 
+    ans=floor((count/5)+(count/25)+(count/125));
+    if(ans==n){
+        cout<<"\nanswer is valid";
+    }
+    else{
+        cout<<"\nAnswer is invalid";
+    }
+
+
 }
