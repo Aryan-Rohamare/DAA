82c0ac89d0381ef21fcdc39a2717447c83f72ddd
Aryan Rohamare <125646823+Aryan-Rohamare@users.noreply.github.com>
2024-09-11 11:08:11 +0530

    Add files via upload

--- c1.cpp
+++ c1.cpp
@@ -1,3 +1,13 @@
+/*Implement Coin Change problem.
+Statement Given an integer array of coins[ ] of
+size N representing different types of currency and an
+integer sum, The task is to find the number of ways to
+make sum by using different combinations
+from coins[].
+Note: Assume that you have an infinite supply of each
+type of coin.
+Input: sum = 4, coins[] = {1,2,3}, Output: 4
+Input: sum = 10, coins[] = {2, 5, 3, 6} Output: 5*/
 #include<iostream>
 using namespace std;
 
